41aff58b3d432039e943b5959f7629bb84106887
Tyler Sanders <tsanders5902@gmail.com>
2023-05-14 15:45:03 -0400

    Made CSS changes and added explanation heading and footer

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ function App() {
       <div className="barchive-header">
         <img src={barchiveHeader} alt="The Barchive" />
       </div>
+      <h2 className="heading">An archive of show flyers for WORLD FAMOUS rock n' roll venue The Bark FL</h2>
       <div className="flyer-container">
         <FlyerGallery />
       </div>
+      <footer className="footer">Mural art by Stephanie! Barchive logo by Leah!</footer>
     </div>
   );
 }
